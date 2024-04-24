e59c6c4691732f927df02871bf0806f478e6c454
loves <2543604745@qq.com>
2024-04-24 15:19:24 +0800

    配置文件

--- config/enter.go
+++ config/enter.go
@@ -1,7 +1,12 @@
 package config
 
 type Config struct {
-	Mysql  Mysql  `yaml:"mysql"`
-	System System `yaml:"system"`
-	Logger Logger `yaml:"logger"`
+	Mysql    Mysql    `yaml:"mysql"`
+	System   System   `yaml:"system"`
+	Logger   Logger   `yaml:"logger"`
+	SiteInfo SiteInfo `yaml:"site_info"`
+	Jwt      Jwt      `yaml:"jwt"`
+	Email    Email    `yaml:"email"`
+	QQ       QQ       `yaml:"qq"`
+	QiNiu    QiNiu    `yaml:"qiniu"`
 }
