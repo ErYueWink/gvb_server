052f642c65e02ef8692146545acbb559f8213e86
loves <2543604745@qq.com>
2024-04-25 21:08:30 +0800

    广告管理-创建广告

---
+++ api/advert_api/advert_create.go
@@ -0,0 +1,43 @@
+package advert_api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gvb_server/global"
+	"gvb_server/models"
+	"gvb_server/utils/res"
+)
+
+type AdvertRequest struct {
+	Title  string `json:"title" struct:"title""`    // 广告标题
+	Href   string `json:"href" struct:"href"`       // 广告跳转链接
+	Images string `json:"images" struct:"images"`   // 广告图片
+	IsShow bool   `json:"is_show" struct:"is_show"` // 是否显示
+}
+
+// AdvertCreateView 创建广告
+func (AdvertApi) AdvertCreateView(c *gin.Context) {
+	var cr AdvertRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		res.FailErrorCode(res.ArgumentError, c)
+		return
+	}
+	var advert models.AdvertModel
+	count := global.DB.Take(&advert, "title = ?", cr.Title).RowsAffected
+	if count != 0 {
+		res.FailWithMsg(fmt.Sprintf("广告%s重复", cr.Title), c)
+		return
+	}
+	err = global.DB.Create(&models.AdvertModel{
+		Title:  cr.Title,
+		Href:   cr.Href,
+		Images: cr.Images,
+		IsShow: cr.IsShow,
+	}).Error
+	if err != nil {
+		res.FailWithMsg("创建广告失败", c)
+		return
+	}
+	res.OKWithMsg("创建广告成功", c)
+}
